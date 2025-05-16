469372fc248349505ce23f11315a487943ba3c46
Mikhail Anishchenko <misha321654897i@gmail.com>
2025-05-16 22:48:52 +0300

    return status 404 in EntityNotFoundException

--- src/main/kotlin/com/team1/coworkings/exception/EntityNotFoundException.kt
+++ src/main/kotlin/com/team1/coworkings/exception/EntityNotFoundException.kt
@@ -1,4 +1,7 @@
 package com.team1.coworkings.exception
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class EntityNotFoundException(entityName : String) :
     RuntimeException("Сущность $entityName не найдена") {}
